6e17df3c3b3a0bf83341dbb0cda24cde343e25fe
Matthew Urrea <matt.urrea.code@gmail.com>
2020-02-05 13:23:34 -0700

    finished oop phase 2

--- php/Classes/Author.php
+++ php/Classes/Author.php
@@ -312,20 +312,20 @@ class Author {
       }
 
       // create query template
-      $query = "SELECT authorId, tweetProfileId  , tweetContent, tweetDate FROM tweet WHERE tweetId = :tweetId";
+      $query = "SELECT authorId, authorActivationToken, authorAvatarUrl, authorEmail, authorHash, authorUsername FROM author WHERE authorId = :authorId";
       $statement = $pdo->prepare($query);
 
-      // bind the tweet id to the place holder in the template
-      $parameters = ["tweetId" => $tweetId->getBytes()];
+      // bind the author id to the place holder in the template
+      $parameters = ["authorId" => $authorId->getBytes()];
       $statement->execute($parameters);
 
-      // grab the tweet from mySQL
+      // grab the author from mySQL
       try {
-         $tweet = null;
+         $author = null;
          $statement->setFetchMode(\PDO::FETCH_ASSOC);
          $row = $statement->fetch();
          if($row !== false) {
-            $tweet = new Tweet($row["tweetId"], $row["tweetProfileId"], $row["tweetContent"], $row["tweetDate"]);
+            $author = new Author($row["authorId"], $row["authorActivationToken"], $row["authorAvatarUrl"], $row["authorEmail"], $row["authorHash"], $row["authorUsername"]);
          }
       } catch(\Exception $exception) {
          // if the row couldn't be converted, rethrow it
